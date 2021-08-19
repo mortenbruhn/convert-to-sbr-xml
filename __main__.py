209157d5b627d9cc38d207db823dc2af08b25e12
Morten Bruhn <mortenbruhn@users.noreply.github.com>
2021-08-19 11:31:05 +0200

    Reformatted

--- __main__.py
+++ __main__.py
@@ -7,7 +7,7 @@ from sbr.elements import Smses, Calls
 from sbr.utils import format_current_time
 from tree.xml_serialization import serialize
 
-file_timestamp = strftime("%Y%m%d%H%M%S", localtime()) # when the export was started, local time
+file_timestamp = strftime("%Y%m%d%H%M%S", localtime())  # when the export was started, local time
 
 handler = argparse.ArgumentParser()
 handler.add_argument('infile', nargs='+')
@@ -19,18 +19,18 @@ smscollection = None if (args.type == 'calls') else Smses()
 callcollection = None if (args.type == 'smses') else Calls()
 
 for infilename in args.infile:
-  parser = get_parser(infilename)
-  with open(infilename, 'rb') as infile:
-    parser.parse(infile, sms_list=smscollection, call_list = callcollection)
+    parser = get_parser(infilename)
+    with open(infilename, 'rb') as infile:
+        parser.parse(infile, sms_list=smscollection, call_list=callcollection)
 
-collection_timestamp = format_current_time() # when the export was finished, Unix timestamp (milliseconds)
+collection_timestamp = format_current_time()  # when the export was finished, Unix timestamp (milliseconds)
 uuid = uuid4()
 
 for collection in [smscollection, callcollection]:
-  if collection:
-    collection['backup_date'] = collection_timestamp
-    collection['backup_set'] = uuid
-    collection.sort()
-    with open(collection.filename_prefix() + file_timestamp + ".xml", 'wb') as outfile:
-      xmlstring = serialize(collection)
-      outfile.write(xmlstring)
+    if collection:
+        collection['backup_date'] = collection_timestamp
+        collection['backup_set'] = uuid
+        collection.sort()
+        with open(collection.filename_prefix() + file_timestamp + ".xml", 'wb') as outfile:
+            xmlstring = serialize(collection)
+            outfile.write(xmlstring)
--- tree/xml_serialization.py
+++ tree/xml_serialization.py
@@ -1,33 +1,36 @@
 from xml.etree import ElementTree
 
+
 def _xmlstring(val):
-  if isinstance(val, str):
-    return val
-  try:
-    return str(val)
-  except UnicodeEncodeError:
-    return val
+    if isinstance(val, str):
+        return val
+    try:
+        return str(val)
+    except UnicodeEncodeError:
+        return val
+
 
 def _handle_node(element, node):
-  for attribute_name in node._required_attributes:
-    if attribute_name not in node:
-      raise RuntimeError("Required attribute {} missing".format(attribute_name))
-    element.set(attribute_name, _xmlstring(node[attribute_name]))
-  for attribute_name in node._optional_attributes:
-    if attribute_name in node:
-      element.set(attribute_name, _xmlstring(node[attribute_name]))
-  calculated_attributes = node.calculated_attributes()
-  for attribute_name in calculated_attributes.keys():
-    element.set(attribute_name, _xmlstring(calculated_attributes[attribute_name]))
+    for attribute_name in node._required_attributes:
+        if attribute_name not in node:
+            raise RuntimeError("Required attribute {} missing".format(attribute_name))
+        element.set(attribute_name, _xmlstring(node[attribute_name]))
+    for attribute_name in node._optional_attributes:
+        if attribute_name in node:
+            element.set(attribute_name, _xmlstring(node[attribute_name]))
+    calculated_attributes = node.calculated_attributes()
+    for attribute_name in calculated_attributes.keys():
+        element.set(attribute_name, _xmlstring(calculated_attributes[attribute_name]))
 
-  if node._children:
-    for child in node._children:
-      child_element = ElementTree.SubElement(element, child.__class__.__name__.lower())
-      _handle_node(child_element, child)
+    if node._children:
+        for child in node._children:
+            child_element = ElementTree.SubElement(element, child.__class__.__name__.lower())
+            _handle_node(child_element, child)
 
 
 def serialize(node):
-  root = ElementTree.Element(node.__class__.__name__.lower())
-  _handle_node(root, node)
-  # ElementTree.tostring does not escape \r and minidom (which would have pretty printing) does not even escape \n
-  return ElementTree.tostring(root, encoding='UTF-8').replace(b'\r', b'&#13;')
+    root = ElementTree.Element(node.__class__.__name__.lower())
+    _handle_node(root, node)
+    # ElementTree.tostring does not escape \r and minidom (which would have pretty printing) does not even escape \n
+    #ElementTree.indent(root)
+    return ElementTree.tostring(root, encoding='UTF-8').replace(b'\r', b'&#13;')
